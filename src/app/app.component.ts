b07e0290a26cdd7f97f0c363392a06a0993bb202
ecali <78408765+ecali@users.noreply.github.com>
2024-12-23 16:26:56 +0100

    add template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator';
+
+  buttons = [
+    ['1', '2', '3', '/'],
+    ['4', '5', '6', '*'],
+    ['7', '8', '9', '-'],
+    ['0', '.', '=', '+']
+  ];
+  
+
 }
